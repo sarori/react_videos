c45216134dcc85a9d2bc1a3153695fccc7508dfb
sarori <salo3678@gmail.com>
2020-02-03 23:32:32 +0900

    .

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import SearchBar from "./SearchBar";
 class App extends React.Component {
 	render() {
 		return (
-			<div classNmae="ui container">
+			<div className="ui container">
 				<SearchBar />
 			</div>
 		);
