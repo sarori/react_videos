840e53bdf10a80018591c110636557d9fb9ebd84
sarori <salo3678@gmail.com>
2020-02-06 23:28:59 +0900

    .

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from "axios";
+
 const KEY = "AIzaSyDvEzNWSMHBaKSU5UTgih56l2RfsuyWIBc";
+
+export default axios.create({
+	baseURL: "https://www.googleapis.com/youtube/v3",
+	params: {
+		part: "snippet",
+		type: "video",
+		maxResults: 5,
+		key: `${KEY}`
+	}
+});
